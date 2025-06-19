aad5794b4cea96bf1313fdbae5afc1fc8b14ddf0
Gerardo Tordoya <zherar7ordoya@outlook.com>
2025-06-19 19:52:25 -0300

    Update tabla-alumnos.spec.ts

--- 3. Angular/src/app/components/tabla-alumnos/tabla-alumnos.spec.ts
+++ 3. Angular/src/app/components/tabla-alumnos/tabla-alumnos.spec.ts
@@ -5,18 +5,18 @@ import { TablaAlumnos } from './tabla-alumnos';
 describe('TablaAlumnos', () => {
   let component: TablaAlumnos;
   let fixture: ComponentFixture<TablaAlumnos>;
-
+  
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [TablaAlumnos]
     })
     .compileComponents();
-
+    
     fixture = TestBed.createComponent(TablaAlumnos);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
-
+  
   it('should create', () => {
     expect(component).toBeTruthy();
   });
